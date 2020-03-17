59730d7cd1c179da8b857cd090d81a228d621723
Thomas Bowen <hello@tjbowen.me>
2020-03-17 12:11:08 -0700

    not working useState soln

--- src/Grid.js
+++ src/Grid.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 // const row = 4;
 // const column = 4;
 
+const initialState = () => {const rep = {}; 
+                            for (let i = 0; i < 4; i++){
+                                for (let j = 0; j < 4; j++){
+                                    rep[`${i},${j}`] = false
+                                }
+                            }
+                            return rep;
+                            };
+
+
 function MakeRow(props) {
+    const [matrix, setMatrix] = useState(initialState());
+
+    // const tester = setMatrix({...matrix, "3,3": true})
+
     const column = Array(4).fill().map((i, e) => 
                     <Square key={e} 
-                            column={e} 
-                            row={props.row} /> )
+                            column={e}
+                            row={props.row}
+                            hehe={matrix[`${e},${props.row}`]} /> )
 
     return (
         <div
@@ -26,7 +41,8 @@ const generate = Array(4).fill().map((i,e) =>
 
 
 
-function Grid(props) {
+function Grid() {
+
     return (
         <div style={{ display:'flex', 
                       flexDirection: 'column',
@@ -36,5 +52,6 @@ function Grid(props) {
         </div>
     );
 }
+
 
 export default Grid;
